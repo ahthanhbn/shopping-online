b8df16175f1e7900e90b66f2d257ce271e1c48fd
ahthanhbn <acebn02@gmail.com>
2024-06-19 19:58:52 +0700

    init

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+
+const MyConstants = {
+    DB_SERVER: 'cluster.q2jdzrs.mongodb.net',
+    DB_USER: 'acebn02',
+    DB_PASS: '%4012345',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'NGUYEN_BA_THANH',
+    JWT_EXPIRES: '1241928798412748', // in milliseconds
+    EMAIL_USER: '<email_user>', // gmail service
+    EMAIL_PASS: '<app_pass>'
+  };
+  module.exports = MyConstants;
